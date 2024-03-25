896686d01e7ab51df402e9c37feb52780ea5f6f0
Noxisa <tchmelevski@gmail.com>
2024-03-25 08:09:13 +0200

    New website update

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../more/Banner/Banner';
+import Popular from '../more/Popular/Popular';
 
 const Home = () => {
     return (
         <div>
 <Banner />
+<Popular />
         </div>
     )
 }
