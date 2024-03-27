160cb49d216786fc00eec4d59e9d7af9ef96d75d
Noxisa <tchmelevski@gmail.com>
2024-03-27 22:07:18 +0200

    New details

--- client/src/more/Popular/Popular.jsx
+++ client/src/more/Popular/Popular.jsx
@@ -6,11 +6,18 @@ import Item from '../Item/Item';
 const Popular = () => {
 return (
     <div className='popular'>
-        <h1>Popular</h1>
+        <h1>Popular in women</h1>
         <hr />
         <div className='popular-item'>
             {data_product.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                return <Item 
+                key={i} 
+                id={item.id} 
+                name={item.name} 
+                image={item.image} 
+                new_price={item.new_price} 
+                old_price={item.old_price}
+                />
             })}
         </div>
     </div>
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../more/Banner/Banner';
 import Popular from '../more/Popular/Popular';
+import Offers  from '../more/Offers/Offers';
 
 const Home = () => {
     return (
         <div>
 <Banner />
 <Popular />
+<Offers />
         </div>
     )
 }
